0e64d5189b682227f31c76ba6ffd78e874045315
Diego <Diego@25.37.203.82>
2014-03-24 11:48:22 -0500

    Avance R. comprar carrito
    Commit parcial de prueba

--- carrito.web/src/main/webapp/src/controller/carritoController.js
+++ carrito.web/src/main/webapp/src/controller/carritoController.js
@@ -6,6 +6,8 @@ define(['controller/_carritoController','delegate/carritoDelegate'], function() 
             var self = this;
             //this.selection = App.Controller.SelectionController ({});
             
+            this.formaComprarCarritoTemplate = _.template($('#formaComprarCarrito').html());
+            
             Backbone.on('comprar-carrito',function(params)
             {
                 self.comprarCarrito(params);
@@ -16,15 +18,25 @@ define(['controller/_carritoController','delegate/carritoDelegate'], function() 
         {
             console.log('comprar carrito' + params.id);
             
-            var self = this;
+            var self = this; 
             
-            App.Delegate.CarritoDelegate.comprarCarrito(params.id,function(data)
+            /*App.Delegate.CarritoDelegate.comprarCarrito(params.id,function(data)
             {
                 alert('Comprando carrito...');
                 
             },function(data)
             {
                 Backbone.trigger(self.componentId + '-' + 'error', {event: 'comprar-carrito', view: self, id: params.id, data: data, error: 'Error haciendo la compra'});
+            });*/
+            
+            this._renderFormaComprarCarrito();
+        },
+        
+        _renderFormaComprarCarrito: function() {
+            var self = this;
+            this.$el.slideUp("fast", function() {
+                self.$el.html(self.formaComprarCarritoTemplate({carritos: self.carritoModelList.models, componentId: self.componentId, showEdit : self.showEdit , showDelete : self.showDelete}));
+                self.$el.slideDown("fast");
             });
         }
     });
