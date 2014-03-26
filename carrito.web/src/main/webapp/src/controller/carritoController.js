2d0da7e13aa02085bb0059cdaefdbf565486982b
admin <admin@ISIS2603-11>
2014-03-26 09:52:17 -0500

    Req Cancelar

--- carrito.web/src/main/webapp/src/controller/carritoController.js
+++ carrito.web/src/main/webapp/src/controller/carritoController.js
@@ -1,6 +1,6 @@
 define(['controller/_carritoController','delegate/carritoDelegate'], function() {
     App.Controller.CarritoController = App.Controller._CarritoController.extend({
-        
+            
         postInit: function (options)
         {
             var self = this;
@@ -8,6 +8,8 @@ define(['controller/_carritoController','delegate/carritoDelegate'], function() 
             
             this.formaComprarCarritoTemplate = _.template($('#formaComprarCarrito').html());
             this.compraFinalizadaTemplate = _.template($('#compraFinalizada').html());
+            this.cancelarCompraCarrito = _.template($('#cancelarCompraCarrito').html());
+
             
             Backbone.on('comprar-carrito',function(params)
             {
@@ -18,6 +20,12 @@ define(['controller/_carritoController','delegate/carritoDelegate'], function() 
             {
                 self.finalizarCompraCarrito(params);
             });
+            
+            Backbone.on('cancelar-compra-carrito',function(params)
+            {
+                self.cancelarCompraCarrito(params);
+            });
+            
         },
         
         comprarCarrito: function (params)
@@ -28,21 +36,31 @@ define(['controller/_carritoController','delegate/carritoDelegate'], function() 
             
             /*App.Delegate.CarritoDelegate.comprarCarrito(params.id,function(data)
             {
-                App.Delegate.CarritoDelegate._obtenerFacturas(params.id,function(data)
-                {
-                    self._renderFormaComprarCarrito(params.id, data);
-                    
-                },function(data)
-                {
-                    Backbone.trigger(self.componentId + '-' + 'error', {event: 'comprar-carrito', view: self, id: params.id, data: data, error: 'Error haciendo la compra'});
-                });
+                self._renderFormaComprarCarrito(params.id);
                 
             },function(data)
             {
                 Backbone.trigger(self.componentId + '-' + 'error', {event: 'comprar-carrito', view: self, id: params.id, data: data, error: 'Error haciendo la compra'});
-            });*/
+            });   */
             
             this._renderFormaComprarCarrito(params.id); //<<--PARA PRUEBAS por lo que lo de arriba no está sirviendo
+        },
+        cancelarCompraCarrito: function (params)
+        {
+            console.log('Comprar carrito ' + params.id);
+            
+            var self = this; 
+            
+            /*App.Delegate.CarritoDelegate.comprarCarrito(params.id,function(data)
+            {
+                self._renderFormaComprarCarrito(params.id);
+                
+            },function(data)
+            {
+                Backbone.trigger(self.componentId + '-' + 'error', {event: 'comprar-carrito', view: self, id: params.id, data: data, error: 'Error haciendo la compra'});
+            });   */
+            
+            this._renderCancelarCompraCarrito(params.id); //<<--PARA PRUEBAS por lo que lo de arriba no está sirviendo
         },
         
         finalizarCompraCarrito: function (params)
@@ -54,31 +72,33 @@ define(['controller/_carritoController','delegate/carritoDelegate'], function() 
             /*App.Delegate.CarritoDelegate.finalizarCompra(params.id,function(data)
             {
                 self._renderCompraFinalizada(params.id);
-            
+                
             },function(data)
             {
                 Backbone.trigger(self.componentId + '-' + 'error', {event: 'comprar-carrito', view: self, id: params.id, data: data, error: 'Error haciendo la compra'});
             });*/ 
             
-            
             this._renderCompraFinalizada(params.id); //PARA PRUEBAS
         },
         
-        _renderFormaComprarCarrito: function(id, direcciones) 
-        {
+        _renderFormaComprarCarrito: function(id) {
             var self = this;
-            this.$el.slideUp("fast", function()
-            {
-                self.$el.html(self.formaComprarCarritoTemplate({componentId: self.componentId, idCarrito: id, direcciones: direcciones}));
+            this.$el.slideUp("fast", function() {
+                self.$el.html(self.formaComprarCarritoTemplate({componentId: self.componentId, idCarrito: id}));
+                self.$el.slideDown("fast");
+            });
+        },
+         _renderCancelarCompraCarrito: function(id) {
+            var self = this;
+            this.$el.slideUp("fast", function() {
+                self.$el.html(self.cancelarCompraCarrito({componentId: self.componentId, idCarrito: id}));
                 self.$el.slideDown("fast");
             });
         },
         
-        _renderCompraFinalizada: function(id) 
-        {
+        _renderCompraFinalizada: function(id) {
             var self = this;
-            this.$el.slideUp("fast", function() 
-            {
+            this.$el.slideUp("fast", function() {
                 self.$el.html(self.compraFinalizadaTemplate({componentId: self.componentId, idCarrito: id}));
                 self.$el.slideDown("fast");
             });
