6d675670c0bba4d0d75fbca9edcf1aa3e22aee2a
Diego Montoya <d.montoya10@uniandes.edu.co>
2014-04-09 10:59:12 -0500

    Avance, agregar al carrito 2.0

--- producto.web/target/producto.web-0.0.1-SNAPSHOT/src/controller/productoController.js
+++ producto.web/target/producto.web-0.0.1-SNAPSHOT/src/controller/productoController.js
@@ -1,6 +1,33 @@
 define(['controller/_productoController','delegate/productoDelegate'], function() {
     App.Controller.ProductoController = App.Controller._ProductoController.extend({
+        
+        postInit: function (options)
+        {                        
+            var self = this;
+            
+            this.redirect = _.template($('#redirectCarrito').html());
 
+            Backbone.on('agregar-producto-al-carrito',function(params)
+            {
+                self.agregarProductoAlCarrito(params);
+            });
+        },
+        
+        agregarProductoAlCarrito: function (params)
+        {
+            console.log('Agregar producto ' + params.id);
+            
+            var self = this; 
+            
+            App.Delegate.CarritoDelegate.comprarCarrito(params.id,function(data)
+            {
+                //Se agrego el producto al carrito
+                
+            },function(data)
+            {
+                Backbone.trigger(self.componentId + '-' + 'error', {event: 'agregar-al-carrito', view: self, id: params.id, data: data, error: 'Error agregando producto'});
+            });            
+        },
     });
     return App.Controller.ProductoController;
 }); 
